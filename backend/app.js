0b8e31d944e91467b5ee37dda3474ccea61589db
Manmeet <mnmtsnghh@gmail.com>
2024-07-31 23:49:58 +0530

    added fe

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
@@ -10,6 +11,8 @@ const folderRouter = require('./routes/folderRoutes');
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
