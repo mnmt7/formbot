86c630b11c2cfb1db0327b0e2883bd6e82c34a8f
Manmeet <mnmtsnghh@gmail.com>
2024-08-01 10:42:59 +0530

    deployment 6

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public', 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
